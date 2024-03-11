aaabdb92e349024a2507ad08cb3d9c034d574c6b
Alexmisoi <54952560+Alexmisoi@users.noreply.github.com>
2024-03-11 06:14:04 -0700

    Add files via upload

---
+++ User_Input.py
@@ -0,0 +1,12 @@
+name = input('enter your name?:')
+age = input('enter your age:')
+location = input('enter your location:')
+
+# first approach
+print('Hello',+name, 'you are', age, 'year old and live in', location)
+
+#second approach
+print('second approach')
+print('Hello{} you are{} years old and you live in {}'.format(name, age, location))
+    
+ 
